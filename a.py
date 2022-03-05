248129669cd781e505465389bcba1b9155441b9b
Shihao Cao <shehowcow@gmail.com>
2022-03-05 12:53:38 -0500

    made modifications to a.py

--- a.py
+++ a.py
@@ -1,7 +1,12 @@
 some_list = [1,2,3]
 
 def func(some_list):
-    some_list.append(4)
+    some_list.append(4,7,8,9)
+    
+    def func2():
+        print(some_list)
+        
+    func2()
     print(some_list)
     
 func(some_list)
